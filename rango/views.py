9c8a606ab607a231a50365fae7cd0dcb0ca55a92
Drummond Canham <3002276Cstudent.gla.ac.uk>
2026-01-26 17:41:10 +0000

    Chapter 4 complete

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+    # Construct a dictionary to pass to template engine as its content
+    # Note that the key matches to __ in the template
+    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    # Return a rendered response to send to client.
+    return render(request, "rango/index.html", context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango'>Index</a>")
+    context_dict = {}
+    return render(request, "rango/about.html", context=context_dict)
